fbcd130f5acbaa39c6221fc0297d70f9aa36bf3d
boogie666 <bogdan.bugarschi@visualretailing.com>
2017-08-11 15:05:57 +0300

    fixed crap

--- src/navigator/ios/pixa-controller.cljs
+++ src/navigator/ios/pixa-controller.cljs
@@ -6,13 +6,16 @@
             [cljs.core.async :refer [<!]])
   (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
-
-
-
 (defn offline? [state]
   (boolean (get-in state [:context :offline])))
 
+(defn pt [& messages]
+  (apply println messages)
+  true)
+
 (defn set-offline-status [state status]
+  {:pre [(pt "pre set-offline-status")]
+   :post [(pt "post set-offline-status")]}
   (assoc-in state [:context :offline] status))
 
 (defn current-page [state]
@@ -24,53 +27,41 @@
 (defn go-back [state]
   (update-in state [:context :status] pop))
 
-
 (defn swapm! [value atom]
   (swap! atom (constantly value))
-  #_(go
+  (go
       (let [[ok _] (<! (ls/save! :pixa/model value))]
         (if ok
           (println "Model saved to local storage")
           (println "Error in saveing model to local storage")))))
 
-
 (defn show-team [team-id]
   (print "show-team" team-id)
   (-> @model
-    (go-to [:team team-id])
-    (swapm! model)))
+      (go-to [:team team-id])
+      (swapm! model)))
 
 (do @model)
 
-
 (defn load-topic [topic-id]
   (REST/POST< "/api/1/query/Topic"
-                                 {
-                                   :find [
-                                           :id
-                                           :name
-                                           :questionnaire_id
-                                           {:questionnaire [:id :name :percentage]}
-                                           {:messages [
-                                                        :id
-                                                        :from_store_id
-                                                        :from_user_id
-                                                        :conversation_id
-                                                        :message
-                                                        :last_modified_date
-                                                        {:user [:name :id]}
-                                                        {:store [:name :id]}]}]
-
-
-                                   :where {:id topic-id}
-                                   :pages {:limit 1  :offset 0}}))
-
-
+              {:find [:id
+                      :name
+                      :questionnaire_id
+                      {:questionnaire [:id :name :percentage]}
+                      {:messages [:id
+                                  :from_store_id
+                                  :from_user_id
+                                  :conversation_id
+                                  :message
+                                  :last_modified_date
+                                  {:user [:name :id]}
+                                  {:store [:name :id]}]}] :where {:id topic-id}
+               :pages {:limit 1  :offset 0}}))
 
 (defn maybe-load-topic [state current-path topic-id]
   (if-not (offline? state)
     ()))
-
 
 (defn first-val [x]
   (first (vals x)))
@@ -84,54 +75,70 @@
     (go
       (try
         (-> @model
-          (assoc-in [:selected :project-id] project-id)
-          (go-to [:topic project-id])
-          (assoc-in current-path (first-val (<! (load-topic project-id))))
-          (swapm! model))
+            (assoc-in [:selected :project-id] project-id)
+            (go-to [:topic project-id])
+            (assoc-in current-path (first-val (<! (load-topic project-id))))
+            (swapm! model))
         (catch :default e (println e))))))
-
-
 
 (defn back []
   (print "back")
   (-> @model
-    (go-back)
-    (swapm! model)))
+      (go-back)
+      (swapm! model)))
+
+(defn load-local-model [state [ok? local-value]]
+  (if-not (or ok? (nil? local-value))
+    (do
+      (println "Error in loading model")
+      state)
+    local-value))
+
+
+(defn load-teams []
+  (POST<
+     "/api/1/query/Team"
+     {:find
+      [:id
+       :name
+       {:topics
+        [:id
+         :name
+         :questionnaire_id
+         {:questionnaire [:id :name :percentage]}
+         :last_modified_date]}]
+      :where {}
+      :pages {:limit 20, :offset 0}}))
+
+(defn pp [state & messages]
+  (apply println messages)
+  state)
 
 
 (defn init []
-  (print "init")
-  #_(go-loop []
+  (println "init")
+  (go-loop []
       (-> @model
-        (set-offline-status (= :connected (<! (ls/net-connected))))
-        (swapm! model))
+          (set-offline-status (= :connected (<! (ls/net-connected))))
+          ((fn [state] (swap! model (constantly state))))
+          (offline?)
+          (println "Status"))
       (recur))
-  #_(go
-     (let [[ok? value] (<! (ls/load! :pixa/model))]
-       (if ok?
-         (when-not (nil? value)
-           (swapm! value model))
-         (println "Error in loading model"))))
   (go
-   (let [connected-to-network? true]
-     (if-not connected-to-network?
-       (-> @model
-         (set-offline-status true)
-         (swapm! model))
+    (-> @model
+      (pp "Loaded local shit")
+      (load-local-model (<! (ls/load! :pixa/model)))
+      ((fn [state] (println state) (swap! model (constantly state)))))
 
-       (-> @model
-         (set-offline-status false)
-         (assoc :teams (<! (POST<
-                             "/api/1/query/Team"
-                             {:find
-                              [:id
-                               :name
-                               {:topics
-                                [:id
-                                 :name
-                                 :questionnaire_id
-                                 {:questionnaire [:id :name :percentage]}
-                                 :last_modified_date]}],
-                              :where {},
-                              :pages {:limit 20, :offset 0}})))
-         (swapm! model))))))
+    (let [connected-to-network? true]
+      (if-not connected-to-network?
+        (-> @model
+            (set-offline-status true)
+            (pp "Offline")
+            (swapm! model))
+
+        (-> @model
+            (set-offline-status false)
+            (assoc :teams (<! (load-teams)))
+            (pp "Online")
+            (swapm! model))))))
--- src/navigator/ios/rest.cljs
+++ src/navigator/ios/rest.cljs
@@ -13,7 +13,10 @@
   (let [request (new goog.net.XhrIo)
         result (promise-chan)]
     (events/listen request "complete" (fn []
-                                        (put! result (:data (js->clj (.getResponseJson request) :keywordize-keys true)))))
+                                        (let [response (js->clj (.getResponseJson request) :keywordize-keys true)]
+                                          (put! result (:data response))
+                                          (println "received response" result))))
+    (println "POST" url json)
     (.send request url "POST" (.stringify js/JSON (clj->js json)) #js{"Content-Type" "application/json"
                                                                       "Cookie" token})
     result))
